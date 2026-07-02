85189610092271e881bdd98d7040141017938378
paulodietricheng <paulo.dietrich.eng@gmail.com>
2026-07-02 08:42:28 -0300

    add sram space

---
+++ rtl/sram/sram.sv
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01.07.2026 15:58:15
+// Design Name: 
+// Module Name: sram
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+import tpu_pkg::*;
+
+module sram(
+    
+    );
+endmodule
